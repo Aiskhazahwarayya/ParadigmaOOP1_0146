b5c2faa58c12ecebf8ee6bdce38a33ca9b069ee4
Aiskhazahwarayya <aiskharayya307@gmail.com>
2024-05-20 17:12:25 +0700

    add class jantung beserta member dan fungsi utama untuk menjalankan relasi antara manusia dan jantung

--- JantungdanManusia/JantungdanManusia.cpp
+++ JantungdanManusia/JantungdanManusia.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+#include "JantungHeader.h"
+#include "ManusiaHeader.h"
+
+int main() {
+	manusia* varManusia = new manusia("jona");
+	delete varManusia;
+	return 0;
+}
 
 
